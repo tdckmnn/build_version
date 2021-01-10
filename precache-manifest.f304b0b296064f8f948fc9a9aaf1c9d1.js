6fa684c2eaec4ce6e00b74af3520a5c201d725d9
tdckmnn <thies.dieckmann@gmail.com>
2021-01-10 01:45:54 +0100

    Init
    initial commit

---
+++ precache-manifest.f304b0b296064f8f948fc9a9aaf1c9d1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d60c7e892e144b456a",
+    "url": "/css/app.ac095712.css"
+  },
+  {
+    "revision": "17f7081679e7496a8c9f0f619604c034",
+    "url": "/img/addIcon.17f70816.svg"
+  },
+  {
+    "revision": "61f5a0dbf9271389b9bd579418a7b27f",
+    "url": "/img/addIcon_active.61f5a0db.svg"
+  },
+  {
+    "revision": "e6a0f29b2c70e05d9cb75e039db63b91",
+    "url": "/img/exportDataIcon.e6a0f29b.png"
+  },
+  {
+    "revision": "0327edd1e5fa01a77a8a198ba4dbd8ac",
+    "url": "/img/largeAddButton.0327edd1.svg"
+  },
+  {
+    "revision": "b57f0f6ec19b5465f276bd3b4edf8e97",
+    "url": "/img/smallAddButton.b57f0f6e.svg"
+  },
+  {
+    "revision": "1a93a1b6bf9a12cfcea985f61fe84896",
+    "url": "/img/undoIcon.1a93a1b6.svg"
+  },
+  {
+    "revision": "a9eb429e45ff063cdeb922e535177358",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05d60c7e892e144b456a",
+    "url": "/js/app.202e03e6.js"
+  },
+  {
+    "revision": "fe05b125b6ae382c3b8d",
+    "url": "/js/chunk-vendors.a6e022a2.js"
+  },
+  {
+    "revision": "489582c8d7100ed2a7db73c04d38fd48",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
